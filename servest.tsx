0587dd4a97085603979451654d843ce805698d0d
Juan Corral <juan.manuel.corral@hotmail.com>
2022-03-20 00:56:29 -0300

    SERVIDOR DENO CON HTTP

---
+++ servest.tsx
@@ -0,0 +1,31 @@
+// @deno-types="https://deno.land/x/servest@v1.3.1/types/react/index.d.ts"
+import React from "https://dev.jspm.io/react/index.js";
+// @deno-types="https://deno.land/x/servest@v1.3.1/types/react-dom/server/index.d.ts"
+import ReactDOMServer from "https://dev.jspm.io/react-dom/server.js";
+import { createApp } from "https://deno.land/x/servest@v1.3.1/mod.ts";
+
+const app = createApp();
+app.handle("/", async (req) => {
+  await req.respond({
+    status: 200,
+    headers: new Headers({
+      "content-type": "text/html; charset=UTF-8",
+    }),
+    body: ReactDOMServer.renderToString(
+      <html>
+        <head>
+          <meta charSet="utf-8" />
+          <title>servest</title>
+        </head>
+        <body>
+          <h1>Hello Servest!</h1>
+          <h2>
+            Utilizar denon para que, ante un cambio de código, el servidor de
+            reinicie automáticamente.
+          </h2>
+        </body>
+      </html>,
+    ),
+  });
+});
+app.listen({ port: 8080 });
